b6de296d5323de40a4cec74f442def71394f92c4
Semih Dağhan <daghanex@hotmail.com>
2022-10-11 17:37:20 +0300

    Reactstrapi Kullanarak Componentleri oluşturmaya başadım
    CategoryList componentini reactstrap ile listgroup olarak listeledim.

--- intro/src/App.js
+++ intro/src/App.js
@@ -2,12 +2,28 @@ import React from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 function App() {
   return (
     <div>
-      <Navi></Navi>
-      <CategoryList></CategoryList>
-      <ProductList></ProductList>
+      <Container>
+
+        <Row>
+          <Navi></Navi>
+        </Row>
+
+        <Row>
+          <Col xs="3">
+          <CategoryList></CategoryList>
+          </Col>
+          <Col xs="9">
+            <ProductList></ProductList>
+          </Col>
+          
+          
+        </Row>
+
+      </Container>
     </div>
   );
 }
