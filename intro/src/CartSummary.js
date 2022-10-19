0ab9f09fb7c110309cbb7d6d7a8a6977b6fcee66
Semih Dağhan <daghanex@hotmail.com>
2022-10-19 23:44:37 +0300

    Fonksiyonlar içerisinde jsx formatı döndürme
    Eğerki sepete eklenen ürün varsa Navbarda sepete eklenen ürünler ve isimleri listelensin sepete ürün ekli değilse sepete ekli ürün yok ibaresini göstersin şeklinde bir operasyon oluşturdum.

--- intro/src/CartSummary.js
+++ intro/src/CartSummary.js
@@ -5,29 +5,41 @@ import {
   DropdownMenu,
   DropdownItem,
   Badge,
+  NavItem,
+  NavLink,
 } from "reactstrap";
 
 export default class CartSummary extends Component {
-  render() {
+  renderSummary() {
     return (
-      <div>
-        <UncontrolledDropdown nav inNavbar>
-          <DropdownToggle nav caret>
-            Sepete eklenen ürünler - {this.props.cart.length}
-          </DropdownToggle>
-          <DropdownMenu right>
-            {this.props.cart.map((cartItem) => (
-              <DropdownItem key={cartItem.product.id}>
-                {cartItem.product.ProductName}
-                <Badge color="success">{cartItem.quantity}</Badge>
-              </DropdownItem>
-            ))}
+    <UncontrolledDropdown nav inNavbar>
+      <DropdownToggle nav caret>
+        Sepete eklenen ürünler - {this.props.cart.length}
+      </DropdownToggle>
+      <DropdownMenu right>
+        {this.props.cart.map((cartItem) => (
+          <DropdownItem key={cartItem.product.id}>
+            {cartItem.product.ProductName}
+            <Badge color="success">{cartItem.quantity}</Badge>
+          </DropdownItem>
+        ))}
 
-            <DropdownItem divider />
-            <DropdownItem>İptal Et</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </div>
-    );
+        <DropdownItem divider />
+        <DropdownItem>İptal Et</DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+    )
+  }
+  renderEmptyCart(){
+    return(
+        <NavItem>
+            <NavLink>Sepete Eklenmiş Ürün Yok</NavLink>
+        </NavItem>
+    )
+  }
+  render() {
+    return <div>
+        {this.props.cart.length>0?this.renderSummary():this.renderEmptyCart()}
+    </div>;
   }
 }
