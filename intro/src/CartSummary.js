72c67ea926dafaab091d5a83624ed37b9fec7202
Semih Dağhan <daghanex@hotmail.com>
2022-10-19 23:34:43 +0300

    Sepete ürün eklendiğinde eklenen ürünleri listeleme

--- intro/src/CartSummary.js
+++ intro/src/CartSummary.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-  } from "reactstrap";
-  
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+
 export default class CartSummary extends Component {
   render() {
     return (
       <div>
         <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Sepete eklenen ürünler - {this.props.cart.length}
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+          <DropdownToggle nav caret>
+            Sepete eklenen ürünler - {this.props.cart.length}
+          </DropdownToggle>
+          <DropdownMenu right>
+            {this.props.cart.map((cartItem) => (
+              <DropdownItem key={cartItem.product.id}>{cartItem.product.ProductName}</DropdownItem>
+            ))}
+
+            <DropdownItem divider />
+            <DropdownItem>İptal Et</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
       </div>
-    )
+    );
   }
 }
