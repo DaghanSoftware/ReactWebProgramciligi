e3a80dfadeb1fd92e4981ba7ff000914aed9857e
Semih Dağhan <daghanex@hotmail.com>
2022-10-23 23:35:23 +0300

    Form submit
    Form submit edildiğinde ekrana uyarı verme

--- intro/src/FormDemo1.js
+++ intro/src/FormDemo1.js
@@ -6,13 +6,18 @@ export default class
     OnCangeHandler = (event)=>{
         this.setState({userName:event.target.value})
     }
+    onSubmitHandler = (event)=>{
+        event.preventDefault();
+        alert(this.state.userName);
+    }
   render() {
     return (
       <div>
-            <form>
+            <form onSubmit={this.onSubmitHandler}>
                 <h3>User Name</h3>
                 <input onChange={this.OnCangeHandler} type="text" ></input>
                 <h3>User name is {this.state.userName}</h3>
+                <input type="submit" value="Save"></input>
             </form>
       </div>
     )
