0f37b867df5d92f51ccbd263b25fa8c236bdcf77
Semih Dağhan <daghanex@hotmail.com>
2022-10-23 23:51:00 +0300

    Çoklu form elementleriyle çalışmak

--- intro/src/FormDemo1.js
+++ intro/src/FormDemo1.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 export default class 
  extends Component {
-    state = {userName:''}
+    state = {userName:'',city:''}
     OnCangeHandler = (event)=>{
-        this.setState({userName:event.target.value})
+        // this.setState({userName:event.target.value})
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]:value})
     }
     onSubmitHandler = (event)=>{
         event.preventDefault();
@@ -15,8 +18,13 @@ export default class
       <div>
             <form onSubmit={this.onSubmitHandler}>
                 <h3>User Name</h3>
-                <input onChange={this.OnCangeHandler} type="text" ></input>
+                <input name='userName' onChange={this.OnCangeHandler} type="text" ></input>
                 <h3>User name is {this.state.userName}</h3>
+
+                <h3>City</h3>
+                <input name='city' onChange={this.OnCangeHandler} type="text" ></input>
+                <h3>City is {this.state.city}</h3>
+
                 <input type="submit" value="Save"></input>
             </form>
       </div>
