727da8f5dc89e21e0cc7e27274c033bbdffd1f63
Semih Dağhan <daghanex@hotmail.com>
2022-11-14 12:59:01 +0300

    Child Props, Multiple Props

---
+++ EgitimSerisi2/begin-react/src/components/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div>
+
+        <div class="card">
+          <img style={{width:'500px', height:'500px'}} src={props.image} class="card-img-top" alt={props.image} />
+          <div class="card-body">
+            <h5 class="card-title">{props.cardTitle}</h5>
+            <p class="card-text">{props.cardText}</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">{props.updatedTime}</small>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Card;
