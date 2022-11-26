8d2d3af1f00591dcb6face171c56cee3f5a6d738
Semih Dağhan <daghanex@hotmail.com>
2022-11-26 15:46:55 +0300

    React-Router

--- EgitimSerisi2/my-movies/src/components/App.js
+++ EgitimSerisi2/my-movies/src/components/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
-import SearchBar from './SearchBar';
-import MovieList from './MovieList';
-import axios from 'axios';
-import AddMovie from './AddMovie';
- class App extends Component {
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import MovieList from "./MovieList";
+import axios from "axios";
+import AddMovie from "./AddMovie";
+import {
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+class App extends Component {
   state = {
-    movies:[],
-    searchQuery:""
-  }
+    movies: [],
+    searchQuery: "",
+  };
 
   //Fetch Api Üzerinden Get Operasyonu
   // async componentDidMount (){
@@ -17,9 +21,9 @@ import AddMovie from './AddMovie';
   //   this.setState({movies:data});
   // }
 
-  async componentDidMount (){
+  async componentDidMount() {
     const response = await axios.get("http://localhost:3002/movies");
-    this.setState({movies:response.data});
+    this.setState({ movies: response.data });
   }
 
   //Static silme işlemi
@@ -33,7 +37,6 @@ import AddMovie from './AddMovie';
   //     movies:newMovieList
   //   }))
   // }
-
 
   //Fetch APİ ile Silme İşlemi
   // deleteMovie= async (movie) => {
@@ -52,38 +55,51 @@ import AddMovie from './AddMovie';
   // }
 
   //Axiox ile silme işlemi
-  deleteMovie= async (movie) => {
-    axios.delete(`http://localhost:3002/movies/${movie.id}`)
-    const newMovieList = this.state.movies.filter(
-      m=>m.id !== movie.id
-      );
-      //Elimizde bir filim olmasaydı aşağıdaki setStatei kullanmak daha mantıklı olurdu ama elimizde film bilgisi mevcut
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`);
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+    //Elimizde bir filim olmasaydı aşağıdaki setStatei kullanmak daha mantıklı olurdu ama elimizde film bilgisi mevcut
     // this.setState({movies:newMovieList})
-    this.setState(state=>({
-      movies:newMovieList
-    }))
-  }
-  searchMovie = (event)=>{
+    this.setState((state) => ({
+      movies: newMovieList,
+    }));
+  };
+  searchMovie = (event) => {
     // console.log(event.target.value);
-    this.setState({searchQuery:event.target.value})
-  }
+    this.setState({ searchQuery: event.target.value });
+  };
   render() {
-    let filteredMovies = this.state.movies.filter(
-      (movie)=>{
-        return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
-      }
-    )
+    let filteredMovies = this.state.movies.filter((movie) => {
+      return (
+        movie.name
+          .toLowerCase()
+          .indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      );
+    });
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <SearchBar searchMovieProp={this.searchMovie}></SearchBar>
-          </div>
-        </div>
-        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie}></MovieList>
-        <AddMovie></AddMovie>
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <React.Fragment>
+                <div className="row">
+                  <div className="col-lg-12">
+                    <SearchBar searchMovieProp={this.searchMovie}></SearchBar>
+                  </div>
+                </div>
+                <MovieList
+                  movies={filteredMovies}
+                  deleteMovieProp={this.deleteMovie}
+                />
+              </React.Fragment>
+            }
+          />
+          <Route exact path="/add" element={<AddMovie />} />
+        </Routes>
       </div>
-    )
+    );
   }
 }
 export default App;
