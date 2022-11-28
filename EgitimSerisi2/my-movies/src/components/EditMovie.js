26239df0731ca0f038316c66073de8bfcdcb6b7e
Semih Dağhan <daghanex@hotmail.com>
2022-11-28 23:50:41 +0300

    Function component state değişikliği

--- EgitimSerisi2/my-movies/src/components/EditMovie.js
+++ EgitimSerisi2/my-movies/src/components/EditMovie.js
@@ -3,28 +3,43 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 export default function EditMovie() {
 
-  const [name, setName] = useState('');
-  const [rating, setrating] = useState('');
-  const [overview, setoverview] = useState('');
-  const [imageURL, setimageURL] = useState('');
+  // const [name, setName] = useState('');
+  // const [rating, setrating] = useState('');
+  // const [overview, setoverview] = useState('');
+  // const [imageURL, setimageURL] = useState('');
 
   const { id } = useParams();
-
+  const [userState, setUserState] = useState({
+    name: "",
+    rating: "",
+    overview: "",
+    imageURL: ""
+  })
   async function fetchData() {
     // const id = window.location.pathname.replace("/edit/", "")
     // console.log(id)
     const response = await axios.get(`http://localhost:3002/movies/${id}`);
     const movie = response.data;
-    setName(movie.name);
-    setrating(movie.rating);
-    setoverview(movie.overview);
-    setimageURL(movie.imageURL);
+    setUserState({
+      ...userState,
+      name: movie.name,
+      rating:movie.rating,
+      overview:movie.overview,
+      imageURL:movie.imageURL
+     });
+    // userState.name(movie.name);
+    // userState.rating(movie.rating);
+    // userState.overview(movie.overview);
+    // userState.imageURL(movie.imageURL);
   }
-  
-  function onInputChange (e){
+
+  const onInputChange = (e)=>{
     console.log(e.target.name);
-    console.log(e.target.value);
-  }
+    setUserState({
+     ...userState,
+     [e.target.name]: e.target.value
+    });
+ }
 
   useEffect(() => {
     fetchData();
@@ -43,17 +58,17 @@ export default function EditMovie() {
         <div className="row">
           <div className="form-group col-md-10">
             <label htmlFor="inputName">Name</label>
-            <input type="text" className="form-control" name="name" value={name} onChange={onInputChange}/>
+            <input type="text" className="form-control" name="name" value={userState.name} onChange={onInputChange}/>
           </div>
           <div className="form-group col-md-2">
             <label htmlFor="inputRating">Rating</label>
-            <input type="text" className="form-control" name="rating" value={rating} onChange={onInputChange} />
+            <input type="text" className="form-control" name="rating" value={userState.rating} onChange={onInputChange} />
           </div>
         </div>
         <div className="form-row">
           <div className="form-group col-md-12">
             <label htmlFor="inputImage">Image URL</label>
-            <input type="text" className="form-control" name="imageURL" value={imageURL} onChange={onInputChange} />
+            <input type="text" className="form-control" name="imageURL" value={userState.imageURL} onChange={onInputChange} />
           </div>
         </div>
         <div className="form-row">
@@ -63,7 +78,7 @@ export default function EditMovie() {
               className="form-control"
               name="overview"
               rows="5"
-              value={overview}
+              value={userState.overview}
               onChange={onInputChange}
             ></textarea>
           </div>
