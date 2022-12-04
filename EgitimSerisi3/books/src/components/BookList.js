e86b89bd2530f2ec59481b4166007d5593575a72
Semih Dağhan <daghanex@hotmail.com>
2022-12-04 23:49:18 +0300

    Context oluşturuldu - Default Value

--- EgitimSerisi3/books/src/components/BookList.js
+++ EgitimSerisi3/books/src/components/BookList.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
 import './BookList.css';
-import Book from './Book'
-
+import {BookContext} from '../context/BookContext';
  class BookList extends Component {
+
+static contextType = BookContext;
+
   render() {
-    const bookList = this.props.books.map((book,i)=>{
-        return<Book book={book} key={i}></Book>
-    })
+    console.log(this.context)
     return (
       <div>
-        <body id="page-top">
-        <section className="page-section bg-light" id="portfolio">
-            <div className="container">
-                <div className="text-center">
-                    <h2 className="section-heading text-uppercase">Portfolio</h2>
-                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-                <div className="row">
-                {bookList}
-                </div>
-            </div>
-        </section>
-        </body>
-        </div>
+
+      </div>
     )
   }
 }
