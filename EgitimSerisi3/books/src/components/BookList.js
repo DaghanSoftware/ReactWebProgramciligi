280ad40ef393676d991be2a016a536a22c8116d2
Semih Dağhan <daghanex@hotmail.com>
2022-12-04 23:55:35 +0300

    Context üzerinden verileri listeleme

--- EgitimSerisi3/books/src/components/BookList.js
+++ EgitimSerisi3/books/src/components/BookList.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
-import './BookList.css';
-import {BookContext} from '../context/BookContext';
- class BookList extends Component {
-
-static contextType = BookContext;
+import React, { Component } from "react";
+import "./BookList.css";
+import Book from "./Book";
+import { BookContext } from "../context/BookContext";
+class BookList extends Component {
+  static contextType = BookContext;
 
   render() {
-    console.log(this.context)
-    return (
-      <div>
+    const books = this.context;
 
-      </div>
-    )
+    return (
+      <body id="page-top">
+        <section className="page-section bg-light" id="portfolio">
+          <div className="container">
+            <div className="text-center">
+              <h2 className="section-heading text-uppercase">Portfolio</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
+            </div>
+            <div className="row">
+              {books.map((book, i) => {
+                return <Book book={book} key={i}></Book>;
+              })}
+            </div>
+          </div>
+        </section>
+      </body>
+    );
   }
 }
 
