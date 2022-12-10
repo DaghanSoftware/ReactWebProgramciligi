f552e1416b22aea4977d4cfd12777e6750c5da26
Semih Dağhan <daghanex@hotmail.com>
2022-12-10 23:52:36 +0300

    Sayfanın tasarımı oluşturuldu

--- EgitimSerisi3/manage-emp/src/App.js
+++ EgitimSerisi3/manage-emp/src/App.js
@@ -2,7 +2,35 @@ import EmployeeList from "./components/EmployeeList.js";
 function App() {
   return (
     <div className="App">
+      <div className="container-xl">
+	<div className="table-responsive">
+		<div className="table-wrapper">
+			<div className="table-title">
+				<div className="row">
+					<div className="col-sm-6">
+						<h2>Manage <b>Employees</b></h2>
+					</div>
+					<div className="col-sm-6">
+						<a href="#addEmployeeModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></a>					
+					</div>
+				</div>
+			</div>
       <EmployeeList></EmployeeList>
+      <div className="clearfix">
+				<div className="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+				<ul className="pagination">
+					<li className="page-item disabled"><a href="#">Previous</a></li>
+					<li className="page-item"><a href="#" className="page-link">1</a></li>
+					<li className="page-item"><a href="#" className="page-link">2</a></li>
+					<li className="page-item active"><a href="#" className="page-link">3</a></li>
+					<li className="page-item"><a href="#" className="page-link">4</a></li>
+					<li className="page-item"><a href="#" className="page-link">5</a></li>
+					<li className="page-item"><a href="#" className="page-link">Next</a></li>
+				</ul>
+			</div>
+      </div>
+	</div>        
+</div>
     </div>
   );
 }
--- EgitimSerisi3/manage-emp/src/components/EmployeeList.js
+++ EgitimSerisi3/manage-emp/src/components/EmployeeList.js
@@ -3,7 +3,20 @@ import Employee from "./Employee";
 const EmployeeList=()=> {
   return (
     <div>
+      <table className="table table-striped table-hover">
+      <thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Address</th>
+						<th>Phone</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+        <tbody>
       <Employee/>
+      </tbody>
+      </table>
     </div>
   )
 }
