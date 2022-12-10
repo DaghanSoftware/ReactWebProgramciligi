f2c638548bf4493a5e6b94c039b887fb91a038fa
Semih Dağhan <daghanex@hotmail.com>
2022-12-10 23:33:44 +0300

    React Hooks
    Hooks için yeni proje oluşturuldu.

---
+++ EgitimSerisi3/manage-emp/src/App.js
@@ -0,0 +1,10 @@
+import EmployeeList from "./components/EmployeeList.js";
+function App() {
+  return (
+    <div className="App">
+      <EmployeeList></EmployeeList>
+    </div>
+  );
+}
+
+export default App;
---
+++ EgitimSerisi3/manage-emp/src/components/EmployeeList.js
@@ -0,0 +1,11 @@
+import Employee from "./Employee";
+
+const EmployeeList=()=> {
+  return (
+    <div>
+      <Employee/>
+    </div>
+  )
+}
+
+export default EmployeeList;
