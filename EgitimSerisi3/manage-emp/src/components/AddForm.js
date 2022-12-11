a664cb0c91cc57e72cc328913ad56308eed28a58
Semih Dağhan <daghanex@hotmail.com>
2022-12-11 23:47:42 +0300

    Ekleme operasyonu tamamlandı

--- EgitimSerisi3/manage-emp/src/components/AddForm.js
+++ EgitimSerisi3/manage-emp/src/components/AddForm.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import {Button, Form, FormGroup} from 'react-bootstrap'
+import React, { useState } from 'react'
+import {Button, Form} from 'react-bootstrap'
+import { EmployeeContext } from '../contexts/EmployeeContext'
+import { useContext } from 'react'
 const AddForm = () => {
+    const {addEmployee} = useContext(EmployeeContext);
+
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [address,setAddress] = useState("");
+    const [phone,setPhone] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        addEmployee(name,email,address,phone)
+    }
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
         <Form.Group>
-            <Form.Control type='Text' placeholder='Name * ' required/>
+            <Form.Control type='Text' placeholder='Name * ' value={name} onChange={e=>setName(e.target.value)} required/>
         </Form.Group>
 
         <Form.Group>
-            <Form.Control type='email' placeholder='Email * ' required/>
+            <Form.Control type='email' placeholder='Email * ' value={email} onChange={e=>setEmail(e.target.value)} required/>
         </Form.Group>
 
         <Form.Group>
-            <Form.Control as='textarea' placeholder='Address * ' rows={3}/>
+            <Form.Control as='textarea' placeholder='Address * ' value={address} onChange={e=>setAddress(e.target.value)} rows={3}/>
         </Form.Group>
 
         <Form.Group>
-            <Form.Control type='Text' placeholder='Name * ' required/>
+            <Form.Control type='Text' placeholder='Name * ' value={phone} onChange={e=>setPhone(e.target.value)} required/>
         </Form.Group>
 
         <Button variant='success' type='submit'>Add New Employee</Button>
