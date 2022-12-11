8a0fd0b509518733f126572a49f4b61eb2a29f8f
Semih Dağhan <daghanex@hotmail.com>
2022-12-11 23:37:03 +0300

    EmployeeContex içerisinde AddEmployee fonksiyonu oluşturuldu.

--- EgitimSerisi3/manage-emp/src/contexts/EmployeeContext.js
+++ EgitimSerisi3/manage-emp/src/contexts/EmployeeContext.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const EmployeeContext = createContext();
 
 const EmployeeContextProvider = (props) => {
-  const [employees] = useState([
+  const [employees,setEmployees] = useState([
     {
       id: uuidv4(),
       name: "Thomas Hardy",
@@ -41,8 +41,11 @@ const EmployeeContextProvider = (props) => {
     },
   ]);
 
+   const addEmployee = (name,email,address,phone) =>{
+    setEmployees([...employees,{id:uuidv4(),name,email,address,phone}])
+  }
   return (
-    <EmployeeContext.Provider value={{ employees }}>
+    <EmployeeContext.Provider value={{ employees,addEmployee }}>
       {props.children}
     </EmployeeContext.Provider>
   );
